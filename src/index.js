3f85673d42707c934cc57f1b05937391943aad3d
Alexey Boklogov <kashira87@mail.ru>
2021-05-29 19:13:57 +0300

    создал разметку по шаблону

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import './sass/main.scss';
+// import './sass/main.scss';
+import menuCardsTpl from './templates/menu-card.hbs';
+import cards from './JS/menu.json';
+
+const menuContainerEl = document.querySelector('.js-menu');
+const cardsMarkup = createMenuCardsMarkup(cards);
+
+menuContainerEl.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createMenuCardsMarkup(cards) {
+    return cards.map(menuCardsTpl).join('');
+};
