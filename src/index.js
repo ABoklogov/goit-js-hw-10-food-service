c1fa67addfa7326e59b1521b9b903ccdd24bd9a3
Alexey Boklogov <kashira87@mail.ru>
2021-05-29 21:18:25 +0300

    Сделал функционал сохранения темы

--- src/index.js
+++ src/index.js
@@ -10,3 +10,38 @@ menuContainerEl.insertAdjacentHTML('beforeend', cardsMarkup);
 function createMenuCardsMarkup(cards) {
     return cards.map(menuCardsTpl).join('');
 };
+
+// Изменение темы:
+
+const checkboxEl = document.querySelector('.theme-switch__toggle');
+const body = document.querySelector('body');
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+
+if (localStorage.getItem('topic') === null) {
+    body.classList.add(Theme.LIGHT);
+    localStorage.setItem('topic', Theme.LIGHT);
+
+    
+};
+
+if (localStorage.getItem('topic') === Theme.LIGHT) {
+    body.classList.add(Theme.LIGHT);
+} else if (localStorage.getItem('topic') === Theme.DARK) {
+    body.classList.add(Theme.DARK);
+}
+
+checkboxEl.addEventListener('change', onChangeTopicClick);
+
+function onChangeTopicClick() {
+    if (localStorage.getItem('topic') === Theme.LIGHT) {
+        body.classList.replace(Theme.LIGHT, Theme.DARK);
+        localStorage.setItem('topic', Theme.DARK);
+    } else if (localStorage.getItem('topic') === Theme.DARK) {
+        body.classList.replace(Theme.DARK, Theme.LIGHT);
+        localStorage.setItem('topic', Theme.LIGHT);
+    };
+};
